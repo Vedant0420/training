f800f07b1f9b86dc12fe2edbed4e5a66343d13b5
Vedant0420 <vedantkhedkar007@gmail.com>
2025-11-13 16:30:35 +0530

    python

--- Python/Main.py
+++ Python/Main.py
@@ -1,4 +1,29 @@
 # This is a comment
+
+#print command
 print("This is a print command")
-first_name="bombaclat"
+
+#strings
+first_name="nigga" #this is a variable
+random_var="shawrma"
 print(first_name)
+print(f"Hello {first_name}") #f string formatting
+print(f"I liek {random_var}")
+
+#integers concepts 
+age = 19
+print(f"You're {age} old")
+student_count = 120
+print(f"There are {student_count}present in this hall")
+
+#floats concepts
+height = 6.2
+print(f"You're {height}'f tall")
+price= 119
+print(f"The price of this product is ${price}")
+cgpa = 8.45
+print(f"Your cgpa for 1st year is {cgpa}!")
+
+#boolean concepts 
+is_student = True
+print(f"Are you a student? {is_student}")
